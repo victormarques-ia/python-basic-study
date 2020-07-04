13c95858f5fd5885f4215d6f5250589f5f494aaa
Victor Silva <victormarques.ia@gmail.com>
2020-07-04 13:48:10 -0300

    selection_if_elif_else

---
+++ selection_if_elif_else.py
@@ -0,0 +1,11 @@
+"""
+Selection in Python is made using the two keywords ‘if’ and ‘elif’ and else (elseif)
+"""
+
+num1 = 34
+if (num1 > 12):
+  print("Num1 is good")
+elif(num1 > 35):
+  print("Num2 is not gooooo...")
+else:
+  print("Num2 is great")
